2d325506a8af253522e96df0d7ee5516d13ada8c
Andrej Antas <andrej.antas@gmail.com>
2013-12-03 13:15:10 +0100

    wrong route

--- app/js/services.js
+++ app/js/services.js
@@ -7,7 +7,7 @@
 // In this case it is a simple value service.
 angular.module('articlesAppServices', ['ngResource']).
   factory('Article', function($resource) {
-    return $resource('http://localhost\\:3003/api/articles/:articleId', {articleId: '@articleId'}, {
+    return $resource('http://localhost\\:3003/api/v2/articles/:articleId', {articleId: '@articleId'}, {
       // save: {method:'PATCH', isArray: true, headers: {
       //     'Accept': 'application/json, text/plain, */*'
       //   }
